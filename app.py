137171928f3ae27275ccc65f506047108acae924
dtrongkhoa <s3846329@student.rmit.edu.au>
2022-04-22 16:13:23 +1000

    change port to 8080

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def upload():
     return render_template("uploadImg.html",msg =msg)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    # app.run(debug=True)
