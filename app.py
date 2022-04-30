0042167f4c29e3dad7956f20b10d35913307dcb2
dtrongkhoa <s3846329@student.rmit.edu.au>
2022-04-30 11:56:08 +1000

    enter ID for upload image

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 import logging
 app = Flask(__name__)
 import aws_key as keys
+import requests
 
 id = '0';
 s3 = boto3.client('s3',
@@ -23,7 +24,11 @@ def checkImg():
     id = request.form['id']
     try: 
         if s3.head_object(Bucket = BUCKET_NAME, Key = id):
-            msg = 'Exist ID'
+            msg = 'You already have an image, don\'t need to upload anymore'
+            # params={'key': id}
+            # URL='https://3853o61h68.execute-api.us-east-1.amazonaws.com/v1/s3?key=studentimage/0'
+            # r = requests.request("GET", URL, params=params)
+            # print(r)
             # return doMath.html will be implemented later
             return render_template('uploadImg.html', msg=msg)
     except ClientError as e:
@@ -35,7 +40,7 @@ def checkImg():
 
 @app.route('/upload',methods=['post'])
 def upload():
-    
+    id = request.form['id']
     if request.method == 'POST':
         image = request.files['image']
         if image.filename == "":
